347a866d95d7fe897c4c4609ac43843149dcbe70
Matthew Falcioni <mfalcioni1@gmail.com>
2023-10-19 22:24:21 -0400

    lineup collection works now

--- lineup.py
+++ lineup.py
@@ -1,19 +1,54 @@
-from pybaseball import statcast, statcast_batter, batting_stats_bref, team_game_logs
+import pandas as pd
+from pybaseball import statcast, batting_stats_bref
 
-batting_logs = team_game_logs(2023, "PHI", "batting")
+def get_lineups(date, team_abbr):
+    # Fetch Game Data
+    game_data = statcast(date, date)
 
-# Fetch Game Data
-game_data = statcast('2023-10-17', '2023-10-17')
+    game_data = game_data[(game_data['home_team'] == team_abbr) | (game_data['away_team'] == team_abbr)]
 
-# filter to team
-team_data = game_data[(game_data['home_team'] == 'PHI') | (game_data['away_team'] == 'PHI')]
+    # Determine Opposing Team
+    if game_data['home_team'][0] == team_abbr:
+        opp_team_abbr = game_data['away_team'][0]
+        team_data = game_data[game_data['inning_topbot'] == 'Bot']
+        opp_team_data = game_data[game_data['inning_topbot'] == 'Top']
+    else:
+        opp_team_abbr = game_data['home_team'][0]
+        team_data = game_data[game_data['inning_topbot'] == 'Top']
+        opp_team_data = game_data[game_data['inning_topbot'] == 'Bot']
 
-lineup = team_data['batter'].unique()
+    # Fetch Player Data
+    year = int(date[:4])
+    all_batting = batting_stats_bref(year)
 
-# Fetch Player Data
-# pitch-by-pitch data
-#player_data = statcast_batter(start_dt="2023-03-30", end_dt="2023-10-01", player_id=lineup[0])
+    def process_lineup(team_data):
+        # Process Lineup
+        lineup_df = (team_data[['batter', "at_bat_number"]]
+                     .sort_values(by='at_bat_number')
+                     .drop_duplicates(subset='batter', keep='first')
+                     .head(9)  # Get the first 9 batters
+                    )
 
-all_batting = batting_stats_bref(2023)
+        # Merge Lineup and Player Data
+        lineup_with_player_data = pd.merge(
+            lineup_df,
+            all_batting,
+            left_on='batter',
+            right_on='mlbID',
+            how='inner'
+        )
+        return lineup_with_player_data
 
-all_batting[all_batting['mlbID'] == lineup[3]]
+    # Process both lineups
+    lineup_with_player_data = process_lineup(team_data)
+    opp_lineup_with_player_data = process_lineup(opp_team_data)
+
+    return lineup_with_player_data, opp_lineup_with_player_data
+
+if __name__ == '__main__':
+    # Example:
+    date = '2023-10-17'
+    team_abbr = 'PHI'
+    team_lineup, opp_lineup = get_lineups(date, team_abbr)
+    print(team_lineup)
+    print(opp_lineup)
